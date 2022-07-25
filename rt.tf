b47dc1225296f7cdd0063af45832afa012f283fc
Calvin Hyundong Kim <hyuk@microsoft.com>
2022-07-25 19:01:55 +0900

    Add disable_bgp_route_propagation parameter

--- rt.tf
+++ rt.tf
@@ -3,7 +3,8 @@ resource "azurerm_route_table" "rt" {
   name                      = var.name
   location                  = var.location
   resource_group_name       = var.rg
-
+  disable_bgp_route_propagation = var.disable_bgp_route_propagation
+  
   dynamic "route" {
     for_each = var.routes
     
